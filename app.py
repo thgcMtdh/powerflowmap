3f3f447c5fa51e70899315a25bf6b4c96550d61e
thgcMtdh <kyouho315@gmail.com>
2022-06-06 15:31:01 +0900

    upd: 送電線の太さをデータから描画

--- app.py
+++ app.py
@@ -1,1 +1,55 @@
-from flask import Flask
+import pandas as pd
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api, abort
+
+DATA_FOLDER = 'data'  # 送電線情報や潮流情報が格納されているフォルダへのパス
+
+# 送電線情報の読み込み
+df_line = pd.read_csv(DATA_FOLDER + '/csv_akiyouryou_kikan_soudensen.csv')
+
+class Line(Resource):
+    def get(self):
+        """
+        指定したlabelをもつ送電線の情報を取得するAPI. labelに何も指定しない場合、すべての送電線情報を取得する
+        """
+
+        label = request.args.get('label')
+
+        # すべての送電線を返す
+        if label == None:
+            df_res = df_line.dropna(subset=['label'])  # labelがNaNである列は除外
+            return [{
+                'label': df_res.iloc[i]['label'],
+                'name': df_res.iloc[i]['name'],
+                'from': df_res.iloc[i]['from'],
+                'to': df_res.iloc[i]['to'],
+                'voltage': int(df_res.iloc[i]['電圧(kV)']),
+                'capacity': int(df_res.iloc[i]['運用容量値(MW)'])
+            } for i in range(len(df_res.index))]
+
+        # 指定されたlabelの送電線を返す
+        else:
+            df_res = df_line.query('label==@label')  # DataFrameから、labelが一致する行を抜き出す
+            if len(df_res.index) > 0:
+                return {
+                    'label': label, 
+                    'name': df_res.iloc[0]['name'],
+                    'from': df_res.iloc[0]['from'],
+                    'to': df_res.iloc[0]['to'],
+                    'voltage': int(df_res.iloc[0]['電圧(kV)']),
+                    'capacity': int(df_res.iloc[0]['運用容量値(MW)'])
+                }
+            else:
+                return abort(404)  # 指定されたlabelの送電線データがないとき、404
+        
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Line, '/line')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- static/script.js
+++ static/script.js
@@ -1,83 +1,30 @@
 /**
- * 要素定義
+ * サーバから送電線データを受信し、それを基に送電線地図の太さと色を調整する
+ * @param {HTMLElement} svg #mapsvg element
  */
-
-/**
- * @brief 変電所・開閉所などの電気所(node)を格納する
- * @description node のプロパティは以下の通り:
- *     type: "SS" | "SWS",  // SS=変電所,SWS=開閉所
- *     id_bulk: string,     // 基幹系統csvにおける変電所No(開閉所の場合はNoではなく名前にする)
- *     name: string,        // 名前
- *     pos: {
- *         x: number,       // 表示位置x [px]
- *         y: number        // 表示位置y [px]
- *     }
- *     id_local: string,    // 地域供給系統csvにおける変電所No
- *     cap_bulk: number,    // 基幹系統の変圧器運用容量合計[MW]
- *     cap_local: number,   // 地域供給系統の変圧器運用容量合計[MW]
- */
-const nodes = [];
-nodes.push({type: 'SWS', id_bulk: 'kikan-275-shiniwaki', name: '新いわき', pos: {x: 900, y:0}});
-nodes.push({type: 'SS', id_bulk: 'kikan-275-002', name: '新茂木', pos: {x: 900, y:0}});
-nodes.push({type: 'SS', id_bulk: 'kikan-275-003', name: '新栃木', pos: {x: 900, y:0}});
-
-/**
- * @brief 電気所どうしを接続する送電線(line)を格納する
- * @description line のプロパティは以下の通り:
- *     id_bulk: string,  // 基幹系統csvにおける送電線No
- *     name: string,     // 送電線名
- *     voltage: number,  // 電圧[kV]
- *     cap: number,      // 運用容量[MW]
- *     from: string,     // 起点電気所id
- *     to: string        // 終点電気所id
- */
-const lines = [];
-lines.push({id_bulk: 'kikan-500-004', name: '福島幹線(中)', voltage: 500, cap: 3291, from: 'kikan-275-shiniwaki', to: 'kikan-275-002'});
-lines.push({id_bulk: 'kikan-500-010', name: '新茂木線', voltage: 500, cap: 6582, from: 'kikan-275-002', to: 'kikan-275-003'});
-
-
-/**
- * @brief SVG生成
- */
-function createSVG() {
-    // SVGを生成
-    const svg = document.createElementNS('https://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('width', '1024');
-    svg.setAttribute('height', '768');
-    svg.setAttribute('viewBox', '0 0 1024 768');
-    svg.setAttribute('preserveAspectRatio', 'none');
-    svg.setAttribute('style', 'background-color:#eae9e6;')
-    // 送電線描画
-    for (const line of lines) {
-        console.log(line.name);
-    }
-
-    const line = document.createElementNS('https://www.w3.org/2000/svg', 'line');
-    line.setAttribute('x1',20);
-    line.setAttribute('y1',20);
-    line.setAttribute('x2',180);
-    line.setAttribute('y2',180);
-    line.setAttribute('stroke', '#008080');
-    line.setAttribute('stroke-width', 5);
-
-    //svgに追加
-    svg.appendChild( line );
-
-    const p = document.createElement('p');
-    p.innerText = 'てすと';
-
-    // svgをページに追加
-    document.getElementById('container').appendChild(svg);
-    document.getElementById('container').appendChild(p);
+function setLineDesign(svg) {
+    const color = '#919195';
+    $.get('/line', function(lines){
+        if (lines) {
+            const paths = $(svg).find('path');
+            for (var i = 0; i < paths.length; i++) {
+                const label = paths[i].getAttribute('inkscape:label');  // 各pathのinkscape:label属性値を取得
+                if (label) {
+                    const line = lines.find(line => line['label'] === label);  // サーバから受信した送電線データの中から、当該labelをもつ要素を取得
+                    if (line) {
+                        const strokeWidth = line['capacity'] / 250.0;  // 送電線の運用容量に応じて線の太さを変更
+                        paths[i].setAttribute('style',
+                            `fill:none; stroke:${color}; stroke-width:${strokeWidth};`
+                        );
+                    }
+                }
+            }
+        }
+    })
 }
 
-/**
- * 読み込み完了時に実行する処理
- */
-// $(function() {
-//     // createSVG();
-// });
 window.onload = function() {
-    createSVG();
-
+    const mapsvg_wrapper = document.getElementById('mapsvg_wrapper');
+    const mapsvg = mapsvg_wrapper.contentDocument.getElementById('mapsvg');
+    setLineDesign(mapsvg);
 }

cbc77c0d3170eb0fdb5d155fb2d22894d8262e8e
thgcMtdh <kyouho315@gmail.com>
2024-04-27 21:02:05 +0900

    upd: 見た目(ラベルと凡例)の調整

--- powerflowmap_ui/src/components/Legend.vue
+++ powerflowmap_ui/src/components/Legend.vue
@@ -3,88 +3,86 @@ import Line from "./Line.vue";
 import LineNoFlow from "./LineNoFlow.vue";
 import Station from "./Station.vue";
 
-const station = {
-    "name": "変電所",
-    "point": { "x": 89, "y": 1350 },
-    "size": 40,
-    "labelPosition": { "x": 30, "y": 0 }
-};
-const switchstation = {
-    "name": "変電所",
-    "isSwitch": true,
-    "point": { "x": 89, "y": 1400 },
-    "size": 40,
-    "labelPosition": { "x": 30, "y": 0 }
-};
-const line = {
-    "name": "送電線（500kV）",
-    "voltage": 500,
-    "capacity": 6582,
-    "points": [{"x": 350, "y": 550}, {"x": 200, "y": 700}]
-};
-
 </script>
 
 <template>
-    <rect
-        x="50"
-        y="1250"
-        width="700"
-        height="240"
-        fill="rgb(255,255,255)">
-    </rect>
+    <g transform="translate(25,1390)">
+        <rect
+            x="0"
+            y="0"
+            width="1000"
+            height="80"
+            fill="rgb(255,255,255)">
+        </rect>
 
-    <text x="60" y="1300" font-size="30">凡例</text>
+        <text x="10" y="50" font-size="20" font-weight="bold">開閉所</text>
 
-    <Station
-        key="変電所"
-        name="変電所"
-        :is-switch="false"
-        :point="{ 'x': 89, 'y': 1340 }"
-        :size="40"
-        :label-position="{ 'x': 30, 'y': 0 }"
-        :label-size="25"
-    />
+        <Station
+            key="開閉所"
+            name="開閉所"
+            :is-switch="true"
+            :point="{ 'x': 100, 'y': 40 }"
+            :size="40"
+            :label-position="{ 'x': 30, 'y': 0 }"
+        />
 
-    <Station
-        key="開閉所"
-        name="開閉所"
-        :is-switch="true"
-        :point="{ 'x': 89, 'y': 1400 }"
-        :size="40"
-        :label-position="{ 'x': 30, 'y': 0 }"
-        :label-size="25"
-    />
+        <text x="160" y="50" font-size="20" font-weight="bold">変電所</text>
 
-    <Line
-        key="送電線(500kV)(100kV設計)"
-        name="送電線(500kV)(100kV設計)"
-        voltage="1000"
-        capacity="6000"
-        flow="0"
-        :points="[{'x': 300, 'y': 1340}, {'x':400, 'y': 1340}]"
-        :animation-time-step="0"
-    />
-    <Line
-        key="送電線(500kV)"
-        name="送電線(500kV)"
-        voltage="500"
-        capacity="6000"
-        flow="0"
-        :points="[{'x': 300, 'y': 1400}, {'x':400, 'y': 1400}]"
-        :animation-time-step="0"
-    />
-    <Line
-        key="送電線(275kV)"
-        name="送電線(275kV)"
-        voltage="275"
-        capacity="6000"
-        flow="0"
-        :points="[{'x': 300, 'y': 1460}, {'x':400, 'y': 1460}]"
-        :animation-time-step="0"
-    />
-    <text x="410" y="1350" font-size="25">送電線 500kV ※1000kV設計</text>
-    <text x="410" y="1410" font-size="25">送電線 500kV</text>
-    <text x="410" y="1470" font-size="25">送電線 275kV</text>
+        <Station
+            key="変電所"
+            name="変電所"
+            :is-switch="false"
+            :point="{ 'x': 250, 'y': 40 }"
+            :size="40"
+            :label-position="{ 'x': 30, 'y': 0 }"
+        />
 
+        <text x="310" y="50" font-size="20" font-weight="bold">送電線</text>
+
+        <Line
+            key="送電線(500kV)"
+            name="送電線(500kV)"
+            voltage="500"
+            capacity="4000"
+            flow="0"
+            :points="[{'x': 400, 'y': 40}, {'x': 450, 'y': 40}]"
+            :animation-time-step="0"
+        />
+
+        <text x="460" y="50" font-size="20">500kV</text>
+
+        <Line
+            key="送電線(275kV)"
+            name="送電線(275kV)"
+            voltage="275"
+            capacity="4000"
+            flow="0"
+            :points="[{'x': 550, 'y': 40}, {'x': 600, 'y': 40}]"
+            :animation-time-step="0"
+        />
+
+        <text x="610" y="50" font-size="20">275kV</text>
+    
+        <LineNoFlow
+            key="非公開"
+            name="非公開"
+            voltage="500"
+            capacity="4000"
+            flow="0"
+            :points="[{'x': 700, 'y': 40}, {'x': 750, 'y': 40}]"
+            :animation-time-step="0"
+        />
+
+        <LineNoFlow
+            key="非公開"
+            name="非公開"
+            voltage="275"
+            capacity="4000"
+            flow="0"
+            :points="[{'x': 760, 'y': 40}, {'x': 810, 'y': 40}]"
+            :animation-time-step="0"
+        />
+
+        <text x="820" y="50" font-size="20">潮流情報が非公開</text>
+    </g>
 </template>
